f2feab24cae1f35351e2bc9807f7431720159295
XilunWu <wu636@purdue.edu>
2016-03-23 00:22:31 -0400

    add nested example

---
+++ library/src/test/scala/scala/tools/escape/NestedRegion.scala
@@ -0,0 +1,49 @@
+package scala.tools.escape
+
+import scala.util.escape._
+import scala.offheap._
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class NestedRegions extends CompilerTesting {
+	//need modification
+	trait Data[T] { 
+		def size: Long
+		def apply(i: Long)(implicit @local cc: T): Long 
+		def update(i: Long, x:Long)(implicit @local cc: T): Unit
+	}
+	trait Region { 
+		type Cap
+		def alloc(n: Long)(implicit @local c: Cap): Data[Cap] 
+	}
+
+	abstract class F[B] { def apply(r: Region): r.Cap -> B }
+
+	def withRegion[T](n: Long)(f: F[T]): T = {
+		implicit val allocator: Allocator = malloc	
+	  	//pay attention not to access outOfBoundary?	
+	  	object cap
+	  	val data = Array.uninit[Long](n.toInt)
+
+		val r = new Region {
+		  	type Cap = Any
+		    //var data = Array.uninit[Long](n.toInt)	//Result type in structural refinement may not refer to a user-defined value class
+		    											//Solution is to put it outside new Region {} block
+	    	//var data = Array.uninit[Long](n.toInt).toArray
+		    for (i <- 0 until n.toInt) data(i) = 0
+
+	   		var p = 0L
+		    def alloc(n: Long)(implicit @local c: Cap) = new Data[Cap] {
+		    	def size = n
+	    		val addr = p
+	    		p += n
+		    	def apply(i: Long)(implicit @local c: Cap): Long = 
+			    	data((addr+i).toInt)
+		    	def update(i: Long, x:Long)(implicit @local cc: Cap): Unit = 
+		    		data((addr+i).toInt) = x
+		    }
+	    }
+		try f(r)(cap) finally allocator.free(data.addr) //free(r.data)
+	}
+}
