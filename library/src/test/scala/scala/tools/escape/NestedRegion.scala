e490e8418cfc38e42c3e3f5dbe24837d3d010024
Leo Osvald <leo.osvald@gmail.com>
2016-06-16 02:53:00 -0700

    Refactor NestedRegions into a reusable mix-in

--- library/src/test/scala/scala/tools/escape/NestedRegion.scala
+++ library/src/test/scala/scala/tools/escape/NestedRegion.scala
@@ -6,16 +6,17 @@ import scala.offheap._
 import org.junit.Test
 import org.junit.Assert.assertEquals
 
-class NestedRegions extends CompilerTesting {
+trait NestedRegions[LT] { // LT = Any -> use @local, LT = Nothing -> ignore it
+  type plocal = local[LT]
   //need modification
   trait Data[T] {
     def size: Long
-    def apply(i: Long)(implicit @local cc: T): Long
-    def update(i: Long, x:Long)(implicit @local cc: T): Unit
+    def apply(i: Long)(implicit @plocal cc: T): Long
+    def update(i: Long, x:Long)(implicit @plocal cc: T): Unit
   }
   trait Region {
     type Cap
-    def alloc(n: Long)(implicit @local c: Cap): Data[Cap]
+    def alloc(n: Long)(implicit @plocal c: Cap): Data[Cap]
   }
 
   abstract class F[B] { def apply(r: Region): r.Cap -> B }
@@ -26,16 +27,16 @@ class NestedRegions extends CompilerTesting {
     object cap
     val data = Array.uninit[Long](n.toInt) //malloc(n) // FIXME(Xilun) If we put it inside, we would get: Result type in structural refinement may not refer to a user-defined value class
     val r = new Region {
-      type Cap = Any
+      type Cap = cap.type
       var p = 0L
-      def alloc(n: Long)(implicit @local c: Cap) = new Data[Cap] {
-      	def size = n
-      	val addr = p
-      	p += n
-      	def apply(i: Long)(implicit @local c: Cap): Long =
-      	  data((addr+i).toInt)
-      	def update(i: Long, x:Long)(implicit @local cc: Cap): Unit =
-      	  data((addr+i).toInt) = x
+      def alloc(n: Long)(implicit @plocal c: Cap) = new Data[Cap] {
+	def size = n
+	val addr = p
+	p += n
+	def apply(i: Long)(implicit @plocal c: Cap): Long =
+	  data((addr+i).toInt)
+	def update(i: Long, x:Long)(implicit @plocal cc: Cap): Unit =
+	  data((addr+i).toInt) = x
       }
     }
     try f(r)(cap) finally allocator.free(data.addr) //free(r.data)
