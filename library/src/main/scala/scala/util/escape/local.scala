e7706192f6725240bdf2943429d831c950bf6953
Tiark Rompf <tiark.rompf@epfl.ch>
2016-06-16 13:03:55 +0200

    missing file

---
+++ library/src/main/scala/scala/util/escape/local.scala
@@ -0,0 +1,25 @@
+package scala.util.escape
+
+import scala.annotation.{ Annotation, StaticAnnotation, TypeConstraint }
+
+class safe(xs: Any*) extends StaticAnnotation with TypeConstraint
+
+case object %
+
+
+/*
+Any = 2
+Nothing = 1
+*/
+
+class local[-T] extends StaticAnnotation with TypeConstraint
+
+trait `->`[-A,+B] extends Function1[A,B] {
+  def apply(@local[Any] y: A): B
+}
+
+
+
+object ESC {
+    def NO[T](x:T):T = x
+}
